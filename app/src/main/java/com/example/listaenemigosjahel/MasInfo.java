07c9bb19ed33910e3ce608da8a6bcd27740e106a
Jahel <jahelcuadradomanchado@hotmail.com>
2022-08-31 15:33:26 +0200

    Vista detalles personalizada

--- app/src/main/java/com/example/listaenemigosjahel/MasInfo.java
+++ app/src/main/java/com/example/listaenemigosjahel/MasInfo.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MasInfo extends AppCompatActivity {
 
     TextView nombre_tv, sexo_tv, ofensa_tv; //DECLARO LAS VARIABLES
+    ImageView iv_enemigo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,7 @@ public class MasInfo extends AppCompatActivity {
         nombre_tv = (TextView) findViewById(R.id.nombre_tv); //VINCULO LAS VARIABLES CON EL XML
         sexo_tv = (TextView) findViewById(R.id.sexo_tv);
         ofensa_tv = (TextView) findViewById(R.id.ofensa_tv);
+        iv_enemigo = (ImageView) findViewById(R.id.iv_enemigo);
 
 
         Bundle bolsar = getIntent().getExtras(); //RECOJO LOS STRINGS ENVIADOS DESDE LA OTRA ACTIVIDAD
@@ -28,6 +31,13 @@ public class MasInfo extends AppCompatActivity {
         nombre_tv.setText("Nombre: " + bolsar.getString("nombrekey"));
         sexo_tv.setText("Sexo: " + bolsar.getString("sexokey"));
         ofensa_tv.setText("Ofensa: " + bolsar.getString("ofensakey"));
+
+        if(bolsar.getString("sexokey").equals("Masculino")){
+            iv_enemigo.setImageResource(R.drawable.man);
+        }else{
+            iv_enemigo.setImageResource(R.drawable.woman);
+        }
+
     }
 
     public void volverActividadAnterior(View vista) {
